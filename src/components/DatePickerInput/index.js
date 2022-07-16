e3689feef404617ed79b955aab4f0c5bb6807c20
wl-hsu <wlhsu.github@gmail.com>
2022-07-16 04:40:51 -0400

    feat: 🎸 add basic components and initialize register page

---
+++ src/components/DatePickerInput/index.js
@@ -0,0 +1,12 @@
+import datepickerIcon from '../../assets/datepicker-icon.svg';
+import style from './index.module.css';
+
+export default () => (
+  <div className={style.birthdayInput}>
+    <div className={style.birthdayTitleItem}>Date of Birth</div>
+    <div>
+      <span className={style.birthdayPlaceholder}>Year/Month/Day</span>
+      <img className={style.datepickerIcon} src={datepickerIcon} alt="datepickerIcon" />
+    </div>
+  </div>
+);
