7784f4933d45c7b022b19823188de2a327df9d9c
wl-hsu <wlhsu.github@gmail.com>
2022-07-18 17:55:19 -0400

    feat: 🎸 optimize login page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Register from './containers/Register';
+// import Register from './containers/Register';
+import Login from '@containers/Login';
 
 // import { startVconsole } from './utils';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Register />
+    {/* <Register /> */}
+    <Login />
   </React.StrictMode>,
   document.getElementById('root'),
 );
