3c23eb16d9eba779ac49427a2699564aa561c7ba
wl-hsu <wlhsu.github@gmail.com>
2022-07-25 16:28:16 -0400

    feat: 🎸 optimize image checking function

--- src/components/ImageCard/index.js
+++ src/components/ImageCard/index.js
@@ -1,6 +1,8 @@
+import { useState, useRef } from 'react';
 import PropTypes from 'prop-types';
-import { Image } from 'antd-mobile';
+import { Image, ImageViewer } from 'antd-mobile';
 import classNames from 'classnames';
+import Bar from '@components/Bar';
 
 import style from './index.module.scss';
 
@@ -14,7 +16,11 @@ import style from './index.module.scss';
 */
 const ImageCard = ({
   imgs,
+  likesCount,
+  commentsCount,
 }) => {
+  const imageViewRef = useRef();
+  const [visible, setVisible] = useState(false);
   const getWrapper = () => {
     switch (imgs.length) {
       case 1:
@@ -29,17 +35,33 @@ const ImageCard = ({
         return style.wrapper;
     }
   };
+  const onClickImage = (index) => {
+    setVisible(true);
+    imageViewRef.current.swipeTo(index);
+  };
+
   return (
     <div className={style.container}>
       <div className={classNames(style.wrapper, getWrapper())}>
-        {imgs.map((img, index) => (<Image fit="cover" className={classNames(style.img, `img${index}`)} key={classNames(img, index)} src={img} alt="" />))}
+        {imgs.map((img, index) => (<Image onClick={() => onClickImage(index)} fit="cover" className={classNames(style.img, `img${index}`)} key={classNames(img, index)} src={img} alt="" />))}
       </div>
+      <ImageViewer.Multi
+        ref={imageViewRef}
+        images={imgs}
+        visible={visible}
+        onClose={() => {
+          setVisible(false);
+        }}
+      />
+      {visible && <Bar isBottom likesCount={likesCount} commentsCount={commentsCount} />}
     </div>
   );
 };
 
 ImageCard.propTypes = {
   imgs: PropTypes.arrayOf(PropTypes.string),
+  commentsCount: PropTypes.number.isRequired,
+  likesCount: PropTypes.number.isRequired,
 };
 
 ImageCard.defaultProps = {
