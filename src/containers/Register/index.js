85c3365722cac6955e2047ae3e086197879eccfa
wl-hsu <wlhsu.github@gmail.com>
2022-07-25 00:29:23 -0400

    feat: 🎸 use usecontext

--- src/containers/Register/index.js
+++ src/containers/Register/index.js
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Toast } from 'antd-mobile';
-// import Header from '@components/Header';
+import { useNavigate } from 'react-router-dom';
+import { useAppContext } from '@utils/context';
 import { registerUser } from '@services/register';
 import Show from '@components/Show';
 import FirstStep from './components/FirstStep';
@@ -17,7 +18,21 @@ const STEP = {
 const Register = () => {
   const [step, setStep] = useState(STEP.FIRST);
   const [userInfo, setUserInfo] = useState({});
+  const [, setStore] = useAppContext();
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    if (step === STEP.FIRST) {
+      setStore({
+        closeHeaderHandler: () => navigate('/login'),
+      });
+    }
+    if (step === STEP.SECOND) {
+      setStore({
+        closeHeaderHandler: () => setStep(STEP.FIRST),
+      });
+    }
+  }, [step]);
   const gotoNextStepHandler = (data) => {
     setUserInfo(data);
     setStep(STEP.SECOND);
@@ -44,9 +59,10 @@ const Register = () => {
       <Show visible={step === STEP.FIRST}>
         <FirstStep gotoNextStepHandler={gotoNextStepHandler} />
       </Show>
-      <Show visible={step === STEP.SECOND}>
+      <Show visible={step === STEP.SECOND} isMount>
         <SecondStep
           userInfo={userInfo}
+          goToOneStepHandler={() => setStep(STEP.FIRST)}
           confirmRegisterHandler={confirmRegisterHandler}
         />
       </Show>
