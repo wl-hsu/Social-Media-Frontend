20a7b583d085ddba434ab3acf542b99e9c448dec
wl <wlhsu.github@gmail.com>
2022-08-09 08:19:35 -0400

    feat: 🎸 initialize virtualized list

--- src/components/TweetCard/index.js
+++ src/components/TweetCard/index.js
@@ -1,95 +1,60 @@
+import PropTypes from 'prop-types';
 import Bar from '@components/Bar';
+import { OBJECT_KEYS } from '@components/Bar/constants';
 import ImageCard from '@components/ImageCard';
 import { useGoTo } from '@utils/hooks';
 import moment from 'moment';
-import { useState, useEffect } from 'react';
-import { OBJECT_KEYS } from '@components/Bar/constants';
 
 import style from './index.module.scss';
 
-const tweet = {
-  id: 1, // tweet id
-  user: {
-    id: 2, // The user id of the user who sent the tweet
-    username: 'TesterForTweet1', // The username of the user who sent the tweet
-    nickname: 'TFT1', // The nickname of the user who sent the tweet
-    avatar_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRt5wunazzu83CsNiaTIs7iXiGCrzk00_loyw&usqp=CAU.jpg', // 发送该推文的用户头像地址
-  }, // Information about the user who sent the tweet
-  comments: [
-    {
-      id: 1, // comment id
-      tweet_id: 1,
-      user: {
-        id: 1, // the user's id that sent the comment
-        username: 'admin', // The username that sent this comment
-        nickname: null, // The nickname that sent this comment
-        avatar_url: null, // The avatar address of the user who sent the comment
-      }, // Information of the user who sent this comment
-      content: 'Test!', // The text content of this comment
-      created_at: '2022-07-22T15:03:52.662052Z', // when the comment was created
-      likes_count: 0, // Likes for this comment
-      has_liked: false, // Whether the currently logged-in user has liked the comment, true: the currently logged-in user has liked the comment, false: the currently logged-in user has not liked the comment
-    },
-  ], // A collection of comments for this tweet
-  created_at: '2022-07-20T07:38:01.699129Z', // when the tweet was created
-  content: 'Id values are not mutable. Any id value in the body of your PUT or PATCH request will be ignored. Only a value set in a POST request will be respected, but only if not already taken.', // The text content of the tweet
-  likes: [], // who liked the tweet
-  likes_count: 10, // Likes count for this tweet
-  comments_count: 122, // comment counts for this tweet
-  has_liked: false, // Whether the currently logged-in user has liked the tweet, true: the currently logged-in user has liked the tweet, false: the currently logged-in user has not liked the tweet
-  photo_urls: ['https://i.pinimg.com/originals/12/dc/90/12dc90c7703c43981e60fbada8f9f0b2.gif',
-    'https://i.pinimg.com/originals/94/2b/91/942b9161e94d16a86a958b4d50d094fd.gif',
-    'https://thumbs.gfycat.com/TastyBrokenArachnid-size_restricted.gif',
-    'https://thumbs.gfycat.com/AridUncomfortableAnnashummingbird-size_restricted.gif',
-  ], // The set of image addresses for this tweet
-};
-
 /**
-* tweet card
+* Tweet card
 */
-const TweetCard = () => {
-  const [data, setDate] = useState();
+const TweetCard = ({
+  dataSource,
+}) => {
   const go = useGoTo();
-  useEffect(() => {
-    console.log('data', data);
-    setDate([]);
-  }, []);
   return (
     <div className={style.container}>
       <div className={style.avatarContainer}>
-        <img src={tweet.user.avatar_url} alt="avatar" className={style.avatar} />
+        <img src={dataSource.user.avatar_url} alt="avatar" className={style.avatar} />
       </div>
       <div className={style.contentContainer}>
         <div className={style.header}>
           <span className={style.nickname}>
-            {tweet.user.nickname}
+            {dataSource.user.nickname}
           </span>
           @
-          <span className={style.username}>{tweet.user.username}</span>
+          <span className={style.username}>{dataSource.user.username}</span>
           &nbsp;·&nbsp;
-          {moment(tweet.created_at).format('MMM DD')}
+          {moment(dataSource.created_at).format('mm')}
         </div>
-        <div className={style.content} onClick={() => go('tweet', { id: tweet.id })}>
-          {tweet.content}
+        <div className={style.content} onClick={() => go('tweet', { id: dataSource.id })}>
+          {dataSource.content}
         </div>
         <div className={style.photo}>
           <ImageCard
-            imgs={tweet.photo_urls}
-            commentsCount={tweet.comments_count}
-            likesCount={tweet.likes_count}
+            imgs={dataSource.photo_urls}
+            commentsCount={dataSource.comments_count}
+            likesCount={dataSource.likes_count}
           />
         </div>
         <div className={style.bar}>
           <Bar
-            id={tweet.id}
-            commentsCount={tweet.comments_count}
-            likesCount={tweet.likes_count}
+            id={dataSource.id}
+            commentsCount={dataSource.comments_count}
+            likesCount={dataSource.likes_count}
             type={OBJECT_KEYS.TWEET}
           />
         </div>
       </div>
     </div>
   );
+};
+
+TweetCard.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  dataSource: PropTypes.object.isRequired,
 };
 
 export default TweetCard;
--- src/containers/Tweets/index.js
+++ src/containers/Tweets/index.js
@@ -1,22 +1,149 @@
 import TweetCard from '@components/TweetCard';
+import {
+  List, CellMeasurer, WindowScroller, CellMeasurerCache,
+} from 'react-virtualized';
 import { useState, useEffect } from 'react';
 
 import style from './index.module.scss';
+
+const tweet = {
+  id: 1, // tweet id
+  user: {
+    id: 2, // The user id of the user who sent the tweet
+    username: 'TesterForTweet1', // The username of the user who sent the tweet
+    nickname: 'TFT1', // The nickname of the user who sent the tweet
+    avatar_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRt5wunazzu83CsNiaTIs7iXiGCrzk00_loyw&usqp=CAU.jpg', // The avatar address of the user who sent the tweet
+  }, // Information about the user who sent the tweet
+  comments: [
+    {
+      id: 1, // comment id
+      tweet_id: 1,
+      user: {
+        id: 1, // the user's id that sent the comment
+        username: 'Mewadmin',
+        nickname: 'Mew',
+        avatar_url: 'https://www.pokemon.cn/play/resources/pokedex/img/pm/3373da1ae6e9a429e7fc8dbad72bf5f4726eb13b.png', // 发送该评论的用户头像地址
+      },
+      content: 'Test!', // The text content of this comment
+      created_at: '2021-12-22T15:03:52.662052Z', // when the comment was created
+      likes_count: 0,
+      has_liked: false, //  Whether the currently logged-in user has liked the comment, true: the currently logged-in user has liked the comment, false: the currently logged-in user has not liked the comment
+    },
+  ],
+  created_at: '2021-12-18T07:38:01.699129Z',
+  content: 'Id values are not mutable. Any id value in the body of your PUT or PATCH request will be ignored. Only a value set in a POST request will be respected, but only if not already taken.', // 该推文的文本内容
+  likes: [],
+  likes_count: 10,
+  comments_count: 122,
+  has_liked: false,
+  photo_urls: ['https://i.pinimg.com/originals/12/dc/90/12dc90c7703c43981e60fbada8f9f0b2.gif',
+    'https://i.pinimg.com/originals/94/2b/91/942b9161e94d16a86a958b4d50d094fd.gif',
+    'https://thumbs.gfycat.com/TastyBrokenArachnid-size_restricted.gif',
+    'https://thumbs.gfycat.com/AridUncomfortableAnnashummingbird-size_restricted.gif',
+  ], // The set of image addresses for this tweet
+};
+
+const tweet1 = {
+  id: 1, // tweet id
+  user: {
+    id: 2, // The user id of the user who sent the tweet
+    username: 'TesterForTweet1', // The username of the user who sent the tweet
+    nickname: 'TFT1', // The nickname of the user who sent the tweet
+    avatar_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRt5wunazzu83CsNiaTIs7iXiGCrzk00_loyw&usqp=CAU.jpg', // The avatar address of the user who sent the tweet
+  }, // Information about the user who sent the tweet
+  comments: [
+    {
+      id: 1,
+      tweet_id: 1,
+      user: {
+        id: 1,
+        username: 'Mewtwoadmin',
+        nickname: 'Mewtwo',
+        avatar_url: null,
+      },
+      content: 'Test2!',
+      created_at: '2021-12-22T15:03:52.662052Z',
+      likes_count: 0,
+      has_liked: false,
+    },
+  ],
+  created_at: '2021-12-18T07:38:01.699129Z',
+  content: 'It is what you do with the gift of life that determines who you are.',
+  likes: [],
+  likes_count: 10,
+  comments_count: 122,
+  has_liked: false,
+  photo_urls: [
+    'https://images.twgreatdaily.com/images/image/_tV/_tVk3nEBnkjnB-0zz829.jpg',
+    'https://cdn.vox-cdn.com/thumbor/CJaRzQSNR_0TysD9YM6GN68XZdo=/0x0:1750x941/920x613/filters:focal(878x316:1158x596):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/63823444/original.0.jpg',
+  ],
+};
+
+const defaultData = [];
+
+for (let i = 0; i < 100; i += 1) {
+  if (Math.random() < 0.5) {
+    defaultData.push(tweet1);
+  } else {
+    defaultData.push(tweet);
+  }
+}
+const cache = new CellMeasurerCache({
+  fixedWidth: true,
+  minHeight: 200,
+});
 /**
-* Tweets Homepage
+* tweet page
 */
 const Tweets = () => {
-  const [data, setDate] = useState();
+  const [data, setDate] = useState(defaultData);
   useEffect(() => {
     console.log('data', data);
-    setDate([]);
+    setDate(defaultData);
   }, []);
+  const noRowsRenderer = () => 'Loading...';
+
+  const rowRenderer = ({
+    key, style: sy, index, parent,
+  }) => (
+    <CellMeasurer
+      cache={cache}
+      columnIndex={0}
+      key={key}
+      rowIndex={index}
+      parent={parent}
+    >
+      {(registerChild) => (
+        <div style={sy} key={key} ref={registerChild}>
+          <TweetCard dataSource={data[index]} />
+        </div>
+      )}
+    </CellMeasurer>
+  );
   return (
     <div className={style.container}>
-      <TweetCard />
-      <TweetCard />
-      <TweetCard />
-      <TweetCard />
+      <WindowScroller>
+        {({
+          height, width, isScrolling, registerChild, onChildScroll, scrollTop,
+        }) => (
+          <div ref={registerChild}>
+            <List
+              isScrolling={isScrolling}
+              onScroll={onChildScroll}
+              scrollTop={scrollTop}
+              autoHeight
+              height={height}
+              deferredMeasurementCache={cache}
+              rowHeight={cache.rowHeight}
+              overscanRowCount={2}
+              noRowsRenderer={noRowsRenderer}
+              rowCount={data.length}
+              rowRenderer={rowRenderer}
+              width={width}
+            />
+          </div>
+        )}
+      </WindowScroller>
     </div>
   );
 };
